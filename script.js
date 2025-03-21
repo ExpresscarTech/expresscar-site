8663e4b14dafc674101461afe837c0bb65c98ece
ExpresscarTech <expresscar.mcn2@gmail.com>
2025-03-21 15:13:06 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function novaOR() {
+  alert("Funcionalidade de criação de OR ainda será implementada!");
+}
+
+function verOrs() {
+  alert("Aqui serão carregadas as ORs da base de dados.");
+}
+
+function verMarcacoes() {
+  alert("Aqui serão carregadas as marcações.");
+}
