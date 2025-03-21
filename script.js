06a97ea432ca5eecc7a90b3b3c43d1785dc562cb
ExpresscarTech <expresscar.mcn2@gmail.com>
2025-03-21 18:17:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -35,13 +35,15 @@ document.getElementById("novaOrForm").addEventListener("submit", function(event)
         dataEntrega: dataEntrega
     };
 
-    fetch("https://script.google.com/macros/s/AKfycbyRBBdH47pEOByOMfADIComYHbxkd9iAg39aSzkTuxpxwVaRhFFo9pEmunoIABesDfd/exec", {
-        method: "POST",
-        mode: "cors",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(dados)
+    fetch("https://script.google.com/macros/s/AKfycbw0bYd31K9OluO3hfsAlBmi0rurJJvmaVk_6sCXA6G0KRSeEbD6giQcXi_seq_PfVd1/exec", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(dados)
+})
+
     })
     .then(response => response.json())
     .then(data => {
